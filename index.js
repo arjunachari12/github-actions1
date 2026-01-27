9df61fdc5cede83d4aa00a42989395f88dc0d049
murughanm <murughan.m@uprisepartners.com>
2026-01-27 17:52:04 +0530

    dependancies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
+const lodash = require('lodash');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
